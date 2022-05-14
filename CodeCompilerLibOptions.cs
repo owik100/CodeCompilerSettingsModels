046945bdefed90722861e9503818bd80cb780f3a
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2022-05-14 18:23:33 +0200

    Add project files.

---
+++ CodeCompilerLibOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCompilerSettingsModels
+{
+    public class CodeCompilerLibOptions
+    {
+        public string InputPath { get; set; }
+        public string OutputPath { get; set; }
+        public bool BuildToConsoleApp { get; set; }
+    }
+}
---
+++ WorkerServiceOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeCompilerSettingsModels
+{
+    public class WorkerServiceOptions
+    {
+        public int Interval { get; set; }
+        public int MaxThreads { get; set; }
+        public int InternalBufferSize { get; set; }
+        public bool SendMessagesToManager { get; set; }
+        public int SendMessagesPort { get; set; }
+    }
+}
